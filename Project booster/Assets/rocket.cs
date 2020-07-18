6e56396593c88b9810f752867e4602e6d2c0c541
zodiacalien <salxichones@hotmail.com>
2020-07-18 15:10:13 +0100

    Function of force when input is space to go up placed and working. Mass set to 2.

---
+++ Project booster/Assets/rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+       if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+       if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate left");
+        }
+       else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate Right");
+        }
+    }
+}
