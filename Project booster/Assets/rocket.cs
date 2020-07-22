88bfb3600c5efca43e9a2a28ec6e20bf37fc8744
zodiacalien <salxichones@hotmail.com>
2020-07-22 11:16:31 +0100

    More stuff.

--- Project booster/Assets/rocket.cs
+++ Project booster/Assets/rocket.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class rocket : MonoBehaviour
 {
